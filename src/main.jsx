ace08cc68fb2026906dbe9391a238935bc88906d
Manuxx-05 <alexmerlo228@gmail.com>
2026-07-05 01:03:30 +0200

    Update main.jsx
    Importación de theme.css para habilitar las variables globales de estilos

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+
 import "./App.css";
-import { AppProvider } from "./context/AppContext";
+import "./styles/theme.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
+    <App />
   </React.StrictMode>
 );
