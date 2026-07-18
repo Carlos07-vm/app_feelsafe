5640835407e905b3ad1b3ebc49297186ff68b66c
Carlos07-vm <alonsovasquez2502@gmail.com>
2026-07-18 15:44:56 -0600

    actualizaccion
    cambios y agregacion de otros componentes

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,19 +1,59 @@
-import { createContext, useContext, useState } from "react";
+import { 
+    createContext,
+    useContext,
+    useState,
+    useEffect,
+ } from "react";
 
-import mockUser from "../data/mockUser";
+ import { onAuthStateChanged } from "firebase/auth";
+ import { doc, getDoc } from "firebase/firestore";
+ import { auth, db } from "../services/firebase";
+
+
 
 const AppContext=createContext();
 
 export function AppProvider({children}){
+ const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-const[user,setUser]=useState(mockUser);
+  useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, async (firebaseUser) => {
+         if (firebaseUser) {
+            try {
+                const userRef = doc(db, "users", firebaseUser.uid);
+                const userSnap = await getDoc(userRef);
+
+                 if (userSnap.exists()) {
+                    setUser(userSnap.data());
+                } else {
+                     setUser({
+                        uid: firebaseUser.uid,
+                        nombre: firebaseUser.displayName || "",
+                        correo: firebaseUser.email,
+                        foto: firebaseUser.photoURL || "",
+                        proveedor: firebaseUser.providerData[0]?.providerId || "",
+                        });
+                 }
+                 } catch (error) {
+                    console.error("Error al obtener usuario:", error);
+                 }
+                 } else {
+                    setUser(null);
+                 }
+                 setLoading(false);
+            });
+
+              return () => unsubscribe();
+         }, []);
 
 return(
 
 <AppContext.Provider
 value={{
 user,
-setUser
+setUser,
+loading,
 }}
 >
 
