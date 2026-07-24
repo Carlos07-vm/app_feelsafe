47df434885aaeecbdc19868482fb9cbf230de21e
Carlos07-vm <alonsovasquez2502@gmail.com>
2026-07-23 18:28:26 -0600

    actualizacion
    agregacion de  funciones nuevas  pra guardar en firebase

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,6 +1,9 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyBdyaDgbKwMlQEE9-pL8quejQjbWP9xkcQ",
@@ -15,5 +18,6 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
 
 export default app;
