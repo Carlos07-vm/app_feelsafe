34d2076064481a5c7ad207f5a9ce0a21dec3e82f
Carlos07-vm <alonsovasquez2502@gmail.com>
2026-07-23 18:26:36 -0600

    revicion y cambio
    se revisaron unas funciones y  se corrigieron  y cambio de  lineas de codigo

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -24,14 +24,35 @@ export function AppProvider({children}){
                 const userRef = doc(db, "users", firebaseUser.uid);
                 const userSnap = await getDoc(userRef);
 
-                 if (userSnap.exists()) {
-                    setUser(userSnap.data());
+                   if (userSnap.exists()) {
+
+                        setUser({
+                           uid: firebaseUser.uid,
+                           ...userSnap.data(),
+                           
+
+                           photoURL:
+                           userSnap.data().photoURL ||
+                           firebaseUser.photoURL || "",
+
+                           foto:
+                              userSnap.data().foto ||
+                              firebaseUser.photoURL ||
+                              "",
+
+                           displayName: firebaseUser.displayName || "",
+                           email: firebaseUser.email,
+                        });
+
+                     
                 } else {
                      setUser({
                         uid: firebaseUser.uid,
                         nombre: firebaseUser.displayName || "",
                         correo: firebaseUser.email,
                         foto: firebaseUser.photoURL || "",
+                         photoURL: firebaseUser.photoURL || "",
+                         displayName: firebaseUser.displayName || "",
                         proveedor: firebaseUser.providerData[0]?.providerId || "",
                         });
                  }
